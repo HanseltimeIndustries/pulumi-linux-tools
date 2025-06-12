afc1155a9b2d7ad5d2ae960f59caf8206db0d1c3
hanseltime <hanseltime@gmail.com>
2025-06-12 16:21:58 -0600

    ci: updating publishing

---
+++ bin/ci-publish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+##################################################################################
+#
+# This is a bundled script for calling publishing of changesets since it turns out
+# changesets and yarn monorepos don't play exactly well.  This solves that.
+#
+# This is also because the auto changset action can't handle very complex commands as
+# arguments.
+#
+# This assumes that yarn changeset version has been applied already
+#
+# https://github.com/changesets/changesets/issues/432
+#
+##################################################################################
+
+yarn workspaces foreach --no-private --since npm publish --access public
+yarn changeset tag
